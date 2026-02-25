879cbcd263e8969fce3064eaa9fa32c8f29719f2
Christian Stegemann-Philipps <13996598+cstegemann@users.noreply.github.com>
2026-02-25 09:35:17 +0100

    Fix unresolved Compose weight/fillMaxHeight imports

--- app/src/main/java/com/example/mydayplanner/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/mydayplanner/ui/home/HomeScreen.kt
@@ -9,12 +9,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
